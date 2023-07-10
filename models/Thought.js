b636b3c47747b50ed7c22686646763500d0d85ca
Ani-Kandikattu <anikandikattu@gmail.com>
2023-07-09 22:25:33 -0700

    formatted thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -2,58 +2,60 @@ const { Schema, model, Types } = require("mongoose");
 
 const reactionSchema = new Schema(
   {
-      reactionId: {
-          type: Schema.Types.ObjectId,
-          default: () => new Types.ObjectId()
-      },
-      reactionBody: {
-          type: String,
-          required: true,
-          maxLength: 280
-      },
-      username: {
-          type: String,
-          required: true
-      },
-      createdAt: {
-          type: Date,
-          default: Date.now,
-          get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
-      }
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
   },
-      {
-          toJSON: {
-              getters: true
-          },
-          id: false
-      }
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
 );
 
 const thoughtsSchema = new Schema(
   {
-      thoughtText: {
-          type: String,
-          required: true,
-          minLength: 1,
-          maxLength: 280,
-      },
-      createdAt: {
-          // Moment.js time stamp
-          type: Date,
-          default: Date.now,
-          get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
-      },
-      username: {
-          type: String,
-          required: true,
-      },
-      reactions: [reactionSchema]
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      // Moment.js time stamp
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
   {
-      toJSON: {
-          virtuals: true,
-          getters: true
-      },
-      id: false
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
   }
 );
